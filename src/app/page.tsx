d1e779e857074d3d5ac4c6076e3c447c0096a9ba
atulmalviyav2 <atulmalviyav2@gmail.com>
2024-10-23 13:26:24 +0000

    nextjs template code

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <main className="max-w-5xl mx-auto">
+      <h1 className="text-5xl font-bold">
+          Invoicipedia
+      </h1>
+      <p>
+        <a href="/dashboard">
+        SIgn In
+        </a>
+      </p>
+    </main>
+  );
+}
